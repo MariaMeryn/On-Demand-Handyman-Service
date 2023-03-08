82948b107421f67a2f8ff6caf9562ad60f5a417f
unknown <sameryn7@gmail.com>
2023-03-08 02:03:27 -0800

    first commit

---
+++ database/migrations/2023_03_06_085550_create_servicecatagories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('catagories', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name',100);
+            $table->string('Status')->default('active');
+            $table->string('Image')->nullable();
+            $table->text('Description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('catagories');
+    }
+};
